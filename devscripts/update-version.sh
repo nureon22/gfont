4744aa5093964da5e4208e58e183507813829265
nureon22 <phyowailin2004@proton.me>
2024-06-13 21:01:33 +0630

    Add helper update-version.sh

---
+++ devscripts/update-version.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+if [ -z "$1" ]; then
+    echo -e "\033[31mYou didn't provide any version number\033[0m"
+    exit 1
+fi
+
+sed -i -E "s/^version = .+/version = \"$1\"/" pyproject.toml
+sed -i -E "s/^version = .+/version = \"$1\"/" src/gfont/version.py
