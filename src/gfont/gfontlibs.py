b15e1a05a3780568c614c10a4e8e5704382ceb32
nureon22 <phyowailin2004@proton.me>
2024-06-17 01:57:33 +0630

    Seperate constant variables to seperated file

--- src/gfont/gfontlibs.py
+++ src/gfont/gfontlibs.py
@@ -15,27 +15,10 @@ from typing import (
 from requests import request
 
 from . import utils
+from .constants import *
 
-system_platform = platform.system()
-
-if system_platform == "Linux":
-    FONTS_DIR = os.path.expanduser("~/.local/share/fonts/gfont")
-    CACHE_DIR = os.path.expanduser("~/.cache/gfont")
-    CACHE_FILE = os.path.join(CACHE_DIR, "families.json")
-elif system_platform == "Darwin":
-    FONTS_DIR = os.path.expanduser("~/Library/Fonts/gfont")
-    CACHE_DIR = os.path.expanduser("~/Library/Caches/gfont")
-    CACHE_FILE = os.path.join(CACHE_DIR, "families.json")
-else:
-    raise Exception("You system is not supported yet")
-
-
-REQUEST_TIMEOUT = 10
 IS_ASSUME_YES = False
 IS_NO_CACHE = False
-MAX_WORKERS = 4
-FONT_CACHE_AGE = 3600 * 24 * 30
-BROWSER_USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64; rv:127.0) Gecko/20100101 Firefox/127.0"
 
 
 def get_available_families() -> List[str]:
