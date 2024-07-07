05b457b7a2d9f62055e4ab914d81e3825e2177dc
nureon22 <phyowailin2004@proton.me>
2024-07-07 15:23:43 +0630

    Remove unnecessary devscripts

--- devscripts/update-version.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-if [ -z "$1" ]; then
-    echo -e "\033[31mYou didn't provide any version number\033[0m"
-    exit 1
-fi
-
-sed -i -E "s/^version = .+/version = \"$1\"/" pyproject.toml
-sed -i -E "s/^VERSION = .+/VERSION = \"$1\"/" src/gfont/constants.py
